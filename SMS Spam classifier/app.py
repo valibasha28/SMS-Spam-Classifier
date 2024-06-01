b8571c13fe587b489bb6efa82771baba395bfa86
Vali Basha <79508055+valibasha28@users.noreply.github.com>
2024-06-01 10:18:14 +0530

    Add files via upload

---
+++ SMS Spam classifier/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+model = pickle.load(open('spam_model.pkl', 'rb'))
+cv = pickle.load(open('vector.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    message = request.form['text']
+    data = [message]
+    data = cv.transform(data).toarray()
+    pred = model.predict(data)
+    return render_template('result.html', prediction=pred)
+
+if __name__ == '__main__':
+    app.run(debug=True)
